4a01a76e681ae9f42eff43b023525cbb3b8f6fd3
Desmon <61366533+desmonHak@users.noreply.github.com>
2021-05-01 17:53:11 +0200

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,24 +1,33 @@
 #include "image.h"
+#include <time.h>
+
+int radnom(){
+
+        srand(time(NULL));
+	return rand();
+}
 
 int main()
 {
 
-    image.format = format_ASCII.P1;
 
-    image.size_image[0] = 10;
-    image.size_image[1] = 10;
+    image.format = format_ASCII.P3;
+
+    image.size_image[0] = 1000;
+    image.size_image[1] = 1000;
 
     image.number_colors = "255";
 
     image.name = "output";
 
-    for(int i = 0; i <= image.size_image[0]*image.size_image[1]; i++){
-        write_buffer(i, i, 115);
+    for(uint64_t i = 0; i <= image.size_image[0]*image.size_image[1]; i++){
+	write_buffer(random()%255-1, random()%255-1,random()%255-1);
     }
 
-    write_buffer(255, 0, 0);
+    //write_buffer(255, 0, 0);
     write_image();
     image_info(image.name);
     puts("Programa finalizado.\n");
+    
 
 }
